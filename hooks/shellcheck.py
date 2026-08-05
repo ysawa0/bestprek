26c1221fb799a5c5803934bc7a627fbe0bd9a63b
Yuki Sawa <8774197+ysawa0@users.noreply.github.com>
2026-08-04 19:33:03 -0700

    fix: package managed Python hooks

--- hooks/shellcheck.py
+++ hooks/shellcheck.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-# /// script
-# requires-python = ">=3.12"
-# dependencies = ["shellcheck-py==0.11.0.1"]
-# ///
-
 import os
 import sys
+from typing import NoReturn
 
-os.execvp("shellcheck", ["shellcheck", *sys.argv[1:]])
+
+def main() -> NoReturn:
+    os.execvp("shellcheck", ["shellcheck", *sys.argv[1:]])
